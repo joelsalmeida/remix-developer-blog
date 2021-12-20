2328a1c6c3572167220d29214525afb94ac86f48
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-20 15:08:00 -0300

    Refactoring: Move Posts para um novo modulo, ajusta trechos de codigo

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,23 +1,9 @@
 import { useLoaderData, Link } from 'remix';
-
-export type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts } from '~/post';
+import type { Post } from '~/post';
 
 export const loader = () => {
-  const posts: Post[] = [
-    {
-      slug: 'my-first-post',
-      title: 'My First Post',
-    },
-    {
-      slug: '90s-mixtape',
-      title: 'A Mixtape I Made Just For You',
-    },
-  ];
-
-  return posts;
+  return getPosts();
 };
 
 export default function Posts() {
@@ -28,9 +14,9 @@ export default function Posts() {
       <h1>Posts</h1>
 
       <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link to={post.slug}>{post.title}</Link>
+        {posts.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link to={slug}>{title}</Link>
           </li>
         ))}
       </ul>

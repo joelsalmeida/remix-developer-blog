3ef01c561dcd3c0e7eaa37815de005053e916c2c
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-20 16:11:12 -0300

    Cria funçao getPost, utiliza nova função getPost nas rotas

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,14 +1,19 @@
 import { useLoaderData } from 'remix';
 import type { LoaderFunction } from 'remix';
+import { getPost } from '~/post';
+import invariant from 'tiny-invariant';
 
-export const loader: LoaderFunction = async ({ params }) => params.slug;
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, 'expected params.slug');
+  return getPost(params.slug);
+};
 
 export default function PostSlug() {
-  const slug = useLoaderData();
+  const post = useLoaderData();
 
   return (
     <div>
-      <h1>Post: {slug}</h1>
+      <h1>Post: {post.title}</h1>
     </div>
   );
 }

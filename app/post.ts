a2fd814da0f1cfcc54a653f0aebcfed1ac355797
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-20 15:37:43 -0300

    Fix: Cria type PostMarkdownAttributes

--- app/post.ts
+++ app/post.ts
@@ -1,13 +1,22 @@
 import path from 'path';
 import fs from 'fs/promises';
 import parseFrontMatter from 'front-matter';
+import invariant from 'tiny-invariant';
 
 export type Post = {
   slug: string;
   title: string;
 };
 
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
 const postsPath = path.join(__dirname, '..', 'posts');
+
+function isValidPostAttributes(attributes: any): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
 
 export async function getPosts() {
   const dir = await fs.readdir(postsPath);
@@ -16,6 +25,8 @@ export async function getPosts() {
     dir.map(async (filename) => {
       const file = await fs.readFile(path.join(postsPath, filename));
       const { attributes } = parseFrontMatter(file.toString());
+
+      invariant(isValidPostAttributes(attributes), `${filename} has bad meta data!`);
 
       return {
         slug: filename.replace(/\.md$/, ''),
